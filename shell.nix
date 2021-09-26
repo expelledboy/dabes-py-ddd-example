d265fc01173ddfc207b1b6c9ee43479aa92599d2
expelledboy Anthony Jackson <102334+expelledboy@users.noreply.github.com>
2021-09-26 13:38:22 +0200

    Add nix environment support in vscode

--- shell.nix
+++ shell.nix
@@ -6,8 +6,6 @@ let
   pythonBuildInputs = with pkgs.python39Packages; [
     python
     poetry
-    pip
-    pytest
     venvShellHook
   ];
 in pkgs.mkShell rec {
@@ -19,11 +17,24 @@ in pkgs.mkShell rec {
 
   venvDir = "./.venv";
 
+  # ENV
+  VENV_DIR = venvDir;
+  # https://python-poetry.org/docs/configuration/
+  POETRY_VIRTUALENVS_CREATE = "true";
+  POETRY_VIRTUALENVS_IN_PROJECT = "true";
+  POETRY_VIRTUALENVS_PATH = venvDir;
+
   postVenvCreation = ''
-      poetry install
+    # Set SOURCE_DATE_EPOCH so that we can use python wheels.
+    # This compromises immutability, but is what we need
+    # to allow package installs from PyPI
+    export SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Install python packages
+    poetry install
   '';
 
   shellHook = ''
-      venvShellHook
+    venvShellHook
   '';
 }
